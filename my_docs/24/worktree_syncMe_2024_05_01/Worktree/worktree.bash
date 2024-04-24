b45bb91b47dc6bd9bcfe49beac9fdf0903192624
Robert McLay <mclay@zaniahgroup.com>
2024-04-24 14:13:30 -0500

    new talk notes

---
+++ my_docs/24/worktree_syncMe_2024_05_01/Worktree/worktree.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+# -*- shell-script -*-
+SCRIPT_NAME="${BASH_SOURCE[0]:-${(%):-%x}}"
+SCRIPT_DIR=${SCRIPT_NAME%/*}
+
+
+if [ $SCRIPT_DIR = "." ]; then
+  SCRIPT_DIR=$PWD
+fi  
+
+rm -rf $HOME/ww
+mkdir -p $HOME/ww/lmod
+
+cd $HOME/ww/lmod
+
+runThis ()
+{
+  
+  local cmd=$1
+  echo 
+  echo "% $cmd"
+  echo 
+  eval $cmd
+  #sleep 1
+}
+
+echo
+echo "PWD: $PWD" ; # sleep 1
+runThis "git clone --bare git@github.com:TACC/Lmod.git lmod.git"
+runThis "cd lmod.git"
+
+runThis "git worktree add ../main"
+runThis "git worktree add ../testing"
+runThis "git worktree add ../PR679"
+runThis "tree -L 1 $HOME/ww/lmod"
+runThis "git worktree list"
+
+
