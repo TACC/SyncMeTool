03afc4b1500be7ae0a6a1a4a2dafda5aeea724c7
Robert McLay <mclay@zaniahgroup.com>
2024-04-27 11:37:38 -0500

    updates for 5/1 talk

--- my_docs/24/worktree_syncMe_2024_05_01/Worktree/worktree.bash
+++ my_docs/24/worktree_syncMe_2024_05_01/Worktree/worktree.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 # -*- shell-script -*-
 SCRIPT_NAME="${BASH_SOURCE[0]:-${(%):-%x}}"
 SCRIPT_DIR=${SCRIPT_NAME%/*}
@@ -13,12 +13,29 @@ mkdir -p $HOME/ww/lmod
 
 cd $HOME/ww/lmod
 
+function colorMsg ()
+{
+  local colorName="$1"
+  local str="$2"
+  local NONE=$'\033[0m'
+  declare -A colorT=( ["BLACK"]=$'\033[1;30m'
+           ["RED"]=$'\033[1;31m'
+           ["GREEN"]=$'\033[1;32m'
+           ["YELLOW"]=$'\033[1;33m'
+           ["BLUE"]=$'\033[1;34m'
+           ["MAGENTA"]=$'\033[1;35m'
+           ["CYAN"]=$'\033[1;36m'
+           ["WHITE"]=$'\033[1;37m' )
+  local color="${colorT[$colorName]}"
+  echo "${color}${str}${NONE}"
+}
+
 runThis ()
 {
   
   local cmd=$1
   echo 
-  echo "% $cmd"
+  colorMsg RED "% $cmd"
   echo 
   eval $cmd
   #sleep 1
