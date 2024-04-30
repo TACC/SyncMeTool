54ba685b6b176b82c7896df1192edb9c940f7d7b
Robert McLay <mclay@zaniahgroup.com>
2024-04-30 15:18:50 -0500

    Remove shell function colorMsg; use shell script instead

--- my_docs/24/worktree_syncMe_2024_05_01/Worktree/worktree.bash
+++ my_docs/24/worktree_syncMe_2024_05_01/Worktree/worktree.bash
@@ -13,23 +13,6 @@ mkdir -p $HOME/ww/lmod
 
 cd $HOME/ww/lmod
 
-function colorMsg ()
-{
-  local colorName="$1"
-  local str="$2"
-  local NONE=$'\033[0m'
-  declare -A colorT=( [BLACK]=$'\033[1;30m'
-           [RED]=$'\033[1;31m'
-           [GREEN]=$'\033[1;32m'
-           [YELLOW]=$'\033[1;33m'
-           [BLUE]=$'\033[1;34m'
-           [MAGENTA]=$'\033[1;35m'
-           [CYAN]=$'\033[1;36m'
-           [WHITE]=$'\033[1;37m' )
-  local color="${colorT[$colorName]}"
-  echo "${color}${str}${NONE}"
-}
-
 runThis ()
 {
   
@@ -43,8 +26,8 @@ runThis ()
 
 echo
 echo "PWD: $PWD" ; # sleep 1
-runThis "git clone --bare git@github.com:TACC/Lmod.git lmod.git"
-runThis "cd lmod.git"
+runThis "git clone --bare git@github.com:TACC/Lmod.git bare.git"
+runThis "cd bare.git"
 
 runThis "git worktree add ../main"
 runThis "git worktree add ../testing"
